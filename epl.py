4506dfe87a8bfaeeb4e37ff4584a109a960741b4
3elmiano <melmi785@gmail.com>
2025-08-06 15:55:32 -0700

    Remove unnecessary files from repo

--- epl.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-# Load JSON
-df = pd.read_json('EPL.json')
-
-# Get data for Arsenal
-chelsea_data = df[df["team"] == "Chelsea"]
-print(chelsea_data)
